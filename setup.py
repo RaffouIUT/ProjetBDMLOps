5f7b1a4d2ddf2ecfcdb54171ecbdbf18da075344
RaffouIUT <rafa72don@gmail.com>
2025-02-10 10:06:21 +0100

    maj installations

--- setup.py
+++ setup.py
@@ -8,23 +8,23 @@ with open('README.md','r',encoding="utf-8") as f:
 
 __version__= "0.0.0"
 
-REPO_NAME='mlops_template_project'
-AUTHOR_USER_NAME="asini"
-SRC_REPO = "gridPower"
-AUTHOR_EMAIL="aghilas.sini@univ-lemans.fr"
+REPO_NAME='ProjetBdMlops'
+AUTHOR_USER_NAME="RaffouIUT"
+SRC_REPO = "ProjetBdMlops"
+AUTHOR_EMAIL="Rafael.Doneau.Etu@univ-lemans.fr"
 
 setuptools.setup(
     name=SRC_REPO,
     version=__version__,
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
-    description='a small  python package for  mlops ',
+    description='Projet MLOps ',
     long_description=long_description,
     long_destription_content='text/markdown',
-    url=f"https://git.univ-lemans.fr/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
 
-        "Bug Tracker":f"https://git.univ-lemans.fr/{AUTHOR_USER_NAME}/{REPO_NAME}/",
+        "Bug Tracker":f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/",
 
     },
     package_dir={"":"src"},
